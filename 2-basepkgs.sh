973ef47f566fd3b32f152fb209dfd634c6d71935
mosg93 <79556703+mosg93@users.noreply.github.com>
2023-10-02 06:05:23 +0000

    Update 2-basepkgs.sh

--- 2-basepkgs.sh
+++ 2-basepkgs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Instalar paquetes necesarios
-sudo xbps-install -S void-repo-nonfree util-linux bsdtar coreutils binutils libatomic-devel base-devel curl wget linux-firmware-amd dbus polkit NetworkManager xtools git bat ripgrep fd exa procs helix neovim htop neofetch ntfs-3g p7zip dust fish-shell starship neovim lazygit
+sudo xbps-install -S void-repo-nonfree util-linux bsdtar coreutils binutils libatomic-devel base-devel curl wget linux-firmware-amd dbus polkit NetworkManager xtools git bat ripgrep fd exa procs helix neovim htop neofetch ntfs-3g p7zip dust fish-shell starship neovim lazygit dejavu-fonts-ttf
