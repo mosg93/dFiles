085c943895a7728526b793b694cc0a07b9d281ad
mosg93 <79556703+mosg93@users.noreply.github.com>
2023-10-05 04:13:34 +0000

    Update 4-extra-pkgs.sh

--- 4-extra-pkgs.sh
+++ 4-extra-pkgs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Instalar paquetes necesarios
-sudo xbps-install -S firefox feh mesa-dri vulkan-loader mesa-vulkan-radeon xf86-video-amdgpu xf86-input-libinput mesa-vaapi mesa-vdpau rofi volta zig go xrdb maim xclip Thunar yt-dlp mpv lxappearance volumeicon alsa-utils pulseaudio pavucontrol mcomix firejail
+sudo xbps-install -S firefox feh mesa-dri vulkan-loader mesa-vulkan-radeon xf86-video-amdgpu xf86-input-libinput mesa-vaapi mesa-vdpau rofi volta zig go xrdb maim xclip Thunar yt-dlp mpv lxappearance volumeicon alsa-utils pulseaudio pavucontrol mcomix firejail setxkbmap psmisc
