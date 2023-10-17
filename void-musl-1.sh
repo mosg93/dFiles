0a0c6e142ebd4d5284cd4fdb28489c5a773a30d9
mosg93 <79556703+mosg93@users.noreply.github.com>
2023-10-17 00:07:27 -0600

    Update void-musl-1.sh

--- void-musl-1.sh
+++ void-musl-1.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Nuevo BashRC
+mv ~/dFiles/src/bashrc ~/.bashrc
+mv ~/dFiles/src/alias ~/usr/share/alias
+mv ~/dFiles/src/voidebv ~/usr/share/voidenv
+
 # Paquetes binarios iniciales
 
-sudo xbps-install -S void-repo-nonfree util-linux bsdtar coreutils binutils libatomic-devel base-devel curl wget linux-firmware-amd xtools git bat ripgrep fd exa procs helix neovim htop neofetch ntfs-3g p7zip dust fish-shell lazygit dejavu-fonts-ttf xdg-user-dirs
+sudo xbps-install -S void-repo-nonfree util-linux bsdtar coreutils binutils libatomic-devel base-devel curl wget linux-firmware-amd xtools git bat ripgrep fd exa procs helix neovim htop neofetch ntfs-3g p7zip dust fish-shell lazygit dejavu-fonts-ttf xdg-user-dirs imlib2 imlib2-devel libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel gd-devel freetype-devel xorg-minimal xinit xhost xauth xorg-server xorg-util-macros picom gtk+3 jwm aria2 firefox feh mesa-dri vulkan-loader mesa-vulkan-radeon xf86-video-amdgpu xf86-input-libinput mesa-vaapi mesa-vdpau rofi xrdb maim xclip yt-dlp mpv volumeicon alsa-utils pulseaudio pavucontrol mcomix firejail setxkbmap psmisc rustup zola
 
 # Directorios iniciales
 xdg-user-dirs-update
@@ -15,7 +20,6 @@ mkdir ~/usr/{bin,var,opt,etc,mnt,media,tmp}
 cd ~
 git clone https://github.com/siduck/st.git ~/usr/local/Suckless/st
 git clone https://github.com/void-linux/void-packages.git ~/usr/local/void-packages
-git clone https://github.com/vlang/v ~/usr/local/v
 git clone https://github.com/uditkarode/libxft-bgra ~/usr/share/lib/libxft-bgra
 git clone https://github.com/siduck/chadwm --depth 1  ~/.config/chadwm
 
@@ -23,9 +27,6 @@ cd ~/usr/share/fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip
 wget https://www.wfonts.com/download/data/2016/04/23/symbola/symbola.zip
 
-cd ~/.themes
-wget https://github.com/catppuccin/gtk/releases/download/v0.4.1/Catppuccin-Latte-Standard-Sapphire-Light.zip
-wget https://github.com/catppuccin/gtk/releases/download/v0.4.1/Catppuccin-Mocha-Standard-Sapphire-Dark.zip
 
 cd ~/usr/opt
 wget https://github.com/Kron4ek/Conty/releases/download/1.21.11/conty.sh && chmod +x conty.sh
@@ -33,9 +34,10 @@ ln -s ~/usr/opt/conty.sh ~/usr/bin/Conty
 wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.4.16/Obsidian-1.4.16.AppImage && chmod a+x Obsidian-1.4.16.AppImage
 ln -s ~/usr/opt/Obsidian-1.4.16.AppImage ~/usr/bin/obsidian
 
-cd ~/usr/tmp
-git clone https://aur.archlinux.org/paru.git
-git clone https://github.com/getzola/zola.git
+# Firefox Profile creation
+mkdir -p ~/.mozilla/firefox/Hardened
+cd ~/.mozilla/firefox/Hardened
+curl -o user.js https://codeberg.org/Narsil/user.js/raw/branch/main/desktop/user.js
 
 # Final strap
 cd ~
@@ -47,4 +49,3 @@ mv ~/voidFiles/src/default.conf ~/usr/local/void-packages/etc/default.conf
 mv ~/dFiles/src/chad.mk ~/.config/chadwm/chadwm/config.mk
 mv ~/dFiles/src/chad.defh ~/.config/chadwm/chadwm/config.def.h
 mv ~/dFiles/src/st.mk ~/usr/local/Suckless/st/config.mk
-mv ~/dFiles/src/bashrc ~/.bashrc
