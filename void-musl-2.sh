3449635a2258da8bce4823da20c5daa8e272d935
mosg93 <79556703+mosg93@users.noreply.github.com>
2023-10-16 23:43:09 -0600

    Create void-musl-2.sh

---
+++ void-musl-2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xi imlib2 imlib2-devel libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel gd-devel freetype-devel xorg-minimal xinit xhost xauth xorg-server xorg-util-macros picom gkt+3 jwm
