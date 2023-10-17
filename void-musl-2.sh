c0b312c6902dacc7b2e3e5665e544ee6e0cb456f
mosg93 <79556703+mosg93@users.noreply.github.com>
2023-10-17 00:07:49 -0600

    Delete void-musl-2.sh

--- void-musl-2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xi imlib2 imlib2-devel libXft-devel libX11-devel harfbuzz-devel libXext-devel libXrender-devel libXinerama-devel gd-devel freetype-devel xorg-minimal xinit xhost xauth xorg-server xorg-util-macros picom gkt+3 jwm
